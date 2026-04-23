b4c9418ecd96d5d3186c1d280a5b72ddbee5178e
Sebastian Moretti <moretti2@hive-dc-7-9-50.hive.hpc.ucdavis.edu>
2026-04-23 15:30:26 -0700

    update

--- teamApp/sebcopy.R
+++ teamApp/sebcopy.R
@@ -7,36 +7,36 @@ dataset <- read_csv("Data_S1.csv")
 
 # data loading and one-time processing here
 
+# data loading and one-time processing here
+Data_S1 <- Data_S1 |> mutate(bodymass_life_ratio = Bodymass/Longevity)
+
 
 # Define UI for application 
 ui <- fluidPage( #create the overall page
     #UI code here
-  sidebar(
-    ui <- page_fixed(
-    input_dark_mode(id = "mode"), 
-    textOutput("mode"),
+    
     sidebarPanel(
-      radioButtons("order", #the input variable that the value will go into
+      radioButtons("Order", #the input variable that the value will go into
                    "Choose a order to display:",
                    c("Cetacea", "Adrosoricida", "Artidodactyla","Carnivora",'Cingulata',"Eulipotyphla","Lagomorpha","New_World_monkeys","Old_World_monkeys","Perissodactyla","")    
                    
-      )))),
+      )),
     
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("histogram")
     )
-)
+  )
+
+    
+
 # Define server logic 
 
 #States Color Mode 
 server <- function(input, output) {
-  output$mode <- renderText({
-    paste("You are in", input$mode, "mode.")
-  })
   output$histogram <- renderPlot({
     order <- Data_S1 |>
-      filter(ordering == input$Order)
+      filter(Order == input$Order)
     pl <- ggplot(order, aes(x=bodymass_life_ratio))
     pl + geom_histogram()
   })
